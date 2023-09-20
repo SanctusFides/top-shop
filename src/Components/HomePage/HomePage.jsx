60f0b7b89a1f7662647e72c02b696f6b4dcddf1e
Sanctus <jhn.hines@gmail.com>
2023-09-20 18:13:46 -0500

    Item cards formatted better on home

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,28 +1,26 @@
 import ItemCard from "../ItemCard/ItemCard";
-import HomeCSS from "./HomePage.module.css"
+import HomeCSS from "./HomePage.module.css";
 import useFetch from "react-fetch-hook";
 
-
 export default function HomePage() {
-
-  const { isLoading, data, error} = useFetch(
+  const { isLoading, data, error } = useFetch(
     "https://fakestoreapi.com/products?limit=5"
   );
-    
+
   console.log(data);
   return (
     <div className={HomeCSS.homeBody}>
       <h1>SHOPPING</h1>
       {isLoading && <div>Loading...</div>}
       {error && <div>{`There was an issue loading data - ${error}`}</div>}
-      <ul>
-      {data &&
-          data.map(({ id, title, price, image}) => (
+      <ul className={HomeCSS.recommends}>
+        {data &&
+          data.map(({ id, title, price, image }) => (
             <li key={id}>
               <ItemCard id={id} title={title} price={price} image={image} />
             </li>
           ))}
       </ul>
     </div>
-  )
+  );
 }
--- src/Components/ItemCard/ItemCard.jsx
+++ src/Components/ItemCard/ItemCard.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ItemCardCSS from "./ItemCard.module.css"
+import ItemCardCSS from "./ItemCard.module.css";
 
 // eslint-disable-next-line react/prop-types
-export default function ItemCard({id, title, price, image}) {
+export default function ItemCard({ title, price, image }) {
+  if (title.length > 55) {
+    title = `${title.slice(0, 60)}...`;
+  }
+
   return (
     <div className={ItemCardCSS.cardBody}>
       <img src={image} className={ItemCardCSS.thumbnail} />
-      <p> {title} </p>
-      ${price}
+      <p> {title} </p>${price}
       <button>Add</button>
     </div>
-  )
+  );
 }
