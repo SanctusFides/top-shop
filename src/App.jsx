4bc14c4697fafbe0bd90a6282bf295afb322d40d
Sanctus <jhn.hines@gmail.com>
2023-10-06 15:27:22 -0500

    Completed Assignment - MVP

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,6 @@ import HomePage from './Components/HomePage/HomePage'
 
 function App({cart, handleAddToCart}) {
 
-  // This is for testing the cart
-  // const printCart = async () => {
-  //   if(cart.length === 0) {
-  //     console.log("Cart is empty");
-  //   }
-  //    for (let i = 0; i < cart.length; i++) {
-  //     console.log(cart[i]);
-  //   }
-  // }
-
   function truth() {
     alert(`I am only taking this course for the JS, not web design. 
     
